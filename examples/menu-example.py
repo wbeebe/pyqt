dddfce8d1f85c4fd5e210f6dc45045cc99e8813e
William Beebe <wbeebe@gmail.com>
2019-08-28 22:16:20 -0400

    Added a toolbar to the menu example.

--- examples/menu-example.py
+++ examples/menu-example.py
@@ -16,6 +16,7 @@ import sys
 
 from PyQt5.QtWidgets import (
     QMainWindow,
+    QMessageBox,
     QApplication,
     QWidget,
     QPushButton,
@@ -29,16 +30,19 @@ class App(QMainWindow):
         super().__init__()
         self.setWindowTitle('PyQt5 Menu Example')
         self.setGeometry(100, 100, 640, 480)
+        self.statusBar().showMessage('Message in statusbar.')
 
+        # We want references to the individual menus so that
+        # menu entries can be added to the top level menu entries.
+        #
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('&File')
         editMenu = mainMenu.addMenu('&Edit')
         viewMenu = mainMenu.addMenu('&View')
         searchMenu = mainMenu.addMenu('&Search')
         toolsMenu = mainMenu.addMenu('&Tools')
-        helpMenu = mainMenu.addMenu('&Help')
+        self.MakeHelpMenu(mainMenu)
 
-        #
         # I have a bug here: The icon (QIcon) will not display.
         # I broke up the instatiation to debug this, but nothing
         # works. I'm using Raspbian Buster's 24x24 exit PNG file.
@@ -50,8 +54,37 @@ class App(QMainWindow):
         exitButton.triggered.connect(self.close)
         fileMenu.addAction(exitButton)
 
+        # Make a toolbar. I'm using the same PNG for an icon
+        # I tried to use on the Exit menu selection. It is
+        # displayed here, but not there. *sigh*
+        #
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
+        exitAction.setShortcut('Alt+X')
+        exitAction.triggered.connect(self.close)
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
         self.show()
         sys.exit(app.exec_())
+
+    def MakeHelpMenu(self, mainMenu):
+        helpMenu = mainMenu.addMenu('&Help')
+
+        aboutAction=QAction(QIcon(), 'About', self)
+        aboutAction.setStatusTip('About this application')
+        aboutAction.triggered.connect(self.About)
+        helpMenu.addAction(aboutAction)
+
+        aboutQtAction=QAction(QIcon(), 'About Qt', self)
+        aboutQtAction.setStatusTip('About Qt and Qt Licensing')
+        aboutQtAction.triggered.connect(self.AboutQt)
+        helpMenu.addAction(aboutQtAction)
+
+    def About(self):
+        QMessageBox.information(self, 'About', 'Menu Example 0.0')
+
+    def AboutQt(self):
+        QMessageBox.aboutQt(self, 'About PyQt')
 
 if __name__ == '__main__':
     App()
